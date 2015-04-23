6d7976266c59d44f38314100257b9fb7c2e7fbdc
bogoev <damyan.bogoev@telerik.com>
2015-04-23 17:32:44 +0300

    Adding initial data layer of the flask bookshelf.

---
+++ bookshelf/data/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from bookshelf.data.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    email = Column(String(120), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
