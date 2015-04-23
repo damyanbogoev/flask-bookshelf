3f22b0402301a4cb656e8af4d25605a810e19acb
bogoev <damyan.bogoev@telerik.com>
2015-04-23 10:28:39 +0300

    Improving the application configuration.

--- bookshelf/__init__.py
+++ bookshelf/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask
 from utils import get_instance_folder_path
 from bookshelf.main.controllers import main
 from bookshelf.admin.controllers import admin
+from bookshelf.config import configure_app
 
 app = Flask(__name__,
             instance_path=get_instance_folder_path(),
             instance_relative_config=True)
 
-app.config.from_object('bookshelf.config.DevelopmentConfig')
-app.config.from_pyfile('config.cfg', silent=True)
+configure_app(app)
 
 app.register_blueprint(main, url_prefix='/')
 app.register_blueprint(admin, url_prefix='/admin')
--- bookshelf/config.py
+++ bookshelf/config.py
@@ -1,3 +1,6 @@
+import os
+
+
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
@@ -6,3 +9,20 @@ class BaseConfig(object):
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = True
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+
+config = {
+    "development": "bookshelf.config.DevelopmentConfig",
+    "testing": "bookshelf.config.TestingConfig",
+    "default": "bookshelf.config.DevelopmentConfig"
+}
+
+
+def configure_app(app):
+    config_name = os.getenv('FLAKS_CONFIGURATION', 'default')
+    app.config.from_object(config[config_name])
+    app.config.from_pyfile('config.cfg', silent=True)
