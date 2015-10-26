99e3b8833de204e75fc2b5e02c308ff25db3abf3
Damyan Bogoev <damyan.bogoev@gmail.com>
2015-10-26 23:17:15 +0200

    Adding an Authors view.

--- bookshelf/main/controllers.py
+++ bookshelf/main/controllers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from bookshelf.data.models import Book
+from bookshelf.data.models import Author, Book
 
 
 main = Blueprint('main', __name__, template_folder='templates')
@@ -15,3 +15,10 @@ def display_books():
     books = [book for book in Book.query.all()]
 
     return render_template("books.htm", books=books)
+
+
+@main.route('authors/')
+def display_authors():
+    authors = [author for author in Author.query.all()]
+
+    return render_template("authors.htm", authors=authors)
