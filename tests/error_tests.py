13d1643638bfe92a8cda827e956cef3cd70fa1f9
damyanbogoev <damyan.bogoev@gmail.com>
2015-11-03 23:07:00 +0200

    Adding tests for the application error handlers.

---
+++ tests/error_tests.py
@@ -0,0 +1,39 @@
+from bookshelf import app
+import unittest
+
+class ErrorTests(unittest.TestCase): 
+
+    @classmethod
+    def setUpClass(cls):
+        pass 
+
+    @classmethod
+    def tearDownClass(cls):
+        pass 
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True 
+
+    def tearDown(self):
+        pass 
+
+    def test_pagenotfound_statuscode(self):
+        result = self.app.get('/missing-page') 
+
+        self.assertEqual(result.status_code, 404) 
+
+    def test_pagenotfound_data(self):
+        result = self.app.get('/missing-page') 
+
+        self.assertIn('Page Not Found', result.data)
+
+    def test_unhandledexception_statuscode(self):
+        result = self.app.put('/books') 
+
+        self.assertEqual(result.status_code, 500) 
+
+    def test_unhandledexception_data(self):
+        result = self.app.put('/books') 
+
+        self.assertIn('Something Went Wrong', result.data)
