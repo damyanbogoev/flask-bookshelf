d556a2180be85d1b2d20220cd47189e0e06de724
Damyan Bogoev <damyan.bogoev@gmail.com>
2015-11-03 18:11:24 +0200

    Adding error pages and error logging for unhandled exceptions.

--- bookshelf/__init__.py
+++ bookshelf/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from utils import get_instance_folder_path
 from bookshelf.main.controllers import main
 from bookshelf.admin.controllers import admin
@@ -16,3 +16,18 @@ app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
 app.register_blueprint(main, url_prefix='/')
 app.register_blueprint(admin, url_prefix='/admin')
+
+@app.errorhandler(404)
+def page_not_found(error):
+	app.logger.error('Page not found: %s', (request.path))
+	return render_template('404.htm'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    app.logger.error('Server Error: %s', (error))
+    return render_template('500.htm'), 500
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    app.logger.error('Unhandled Exception: %s', (e))
+    return render_template('500.htm'), 500
