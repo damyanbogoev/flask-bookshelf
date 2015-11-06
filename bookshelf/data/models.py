a4fa6f0feee065dcac160472c68116c98229b82b
Damyan Bogoev <damyan.bogoev@gmail.com>
2015-11-06 21:28:08 +0200

    Adding a description column to the Role.

--- bookshelf/data/models.py
+++ bookshelf/data/models.py
@@ -42,6 +42,7 @@ roles_users = db.Table('roles_users',
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
 
     def __init__(self, name):
         self.name = name
