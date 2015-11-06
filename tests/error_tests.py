12aa59a1b6dadcc752b38075b12b9ad2d97d7561
Damyan Bogoev <damyan.bogoev@gmail.com>
2015-11-06 23:54:37 +0200

    Ignoring docstring linting. Fixing pyling errors.

--- tests/error_tests.py
+++ tests/error_tests.py
@@ -1,39 +1,39 @@
 from bookshelf import app
 import unittest
 
-class ErrorTests(unittest.TestCase): 
+class ErrorTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass 
+        pass
 
     @classmethod
     def tearDownClass(cls):
-        pass 
+        pass
 
     def setUp(self):
         self.app = app.test_client()
-        self.app.testing = True 
+        self.app.testing = True
 
     def tearDown(self):
-        pass 
+        pass
 
     def test_pagenotfound_statuscode(self):
-        result = self.app.get('/missing-page') 
+        result = self.app.get('/missing-page')
 
-        self.assertEqual(result.status_code, 404) 
+        self.assertEqual(result.status_code, 404)
 
     def test_pagenotfound_data(self):
-        result = self.app.get('/missing-page') 
+        result = self.app.get('/missing-page')
 
         self.assertIn('Page Not Found', result.data)
 
-    def test_unhandledexception_statuscode(self):
-        result = self.app.put('/books') 
+    def test_unhandledexception_code(self):
+        result = self.app.put('/books')
 
-        self.assertEqual(result.status_code, 500) 
+        self.assertEqual(result.status_code, 500)
 
     def test_unhandledexception_data(self):
-        result = self.app.put('/books') 
+        result = self.app.put('/books')
 
         self.assertIn('Something Went Wrong', result.data)
