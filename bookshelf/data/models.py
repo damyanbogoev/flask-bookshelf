64ef703ae08a7393c4297a67d60bc635e19c8df2
Damyan Bogoev <damyan.bogoev@gmail.com>
2015-11-06 21:18:02 +0200

    Adding User and Role in the application model.

--- bookshelf/data/models.py
+++ bookshelf/data/models.py
@@ -1,4 +1,5 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import UserMixin, RoleMixin
 
 db = SQLAlchemy()
 
@@ -31,3 +32,34 @@ class Author(db.Model):
 
     def __repr__(self):
         return '<Author %r>' % (self.names)
+
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Role %r>' % (self.name)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.email)
