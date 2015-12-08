01bb05e669db9581655a1539d8bd1126da058c0f
damyanbogoev <damyan.bogoev@gmail.com>
2015-12-08 16:17:31 +0000

    Adding a helthcheck service for the application.

---
+++ check.py
@@ -0,0 +1,30 @@
+from bookshelf.config import config
+from flask import Flask
+from healthcheck import HealthCheck, EnvironmentDump
+import os
+
+app = Flask(__name__)
+config_name = os.getenv('FLAKS_CONFIGURATION', 'default')
+app.config.from_object(config[config_name])
+app.config.from_pyfile('config.cfg', silent=True)
+
+health = HealthCheck(app, "/healthcheck")
+envdump = EnvironmentDump(app, "/environment",
+                          include_python=True, include_os=True,
+                          include_process=False, include_config=True)
+
+def sqlite_available():
+    # TODO: add precise check against the database
+    return True, "sqlite ok"
+
+health.add_check(sqlite_available)
+
+def application_data():
+    return {"maintainer": "Damyan Bogoev",
+            "git_repo": "https://github.com/damyanbogoev/flask-bookshelf"}
+
+envdump.add_section("application", application_data)
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
